53e4a38d42e643bef7ead6c095c46576842bcf56
Beamlak Aschalew <birrletej12@gmail.com>
2024-11-06 21:32:14 +0300

    fixed light reader header

--- controllers/light_reader.ts
+++ controllers/light_reader.ts
@@ -15,8 +15,11 @@ const lightReader = async (req: Request, res: Response) => {
     const response = await axios.get(url as string, {
       headers: {
         "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36",
-        Referer: url,
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
+        "Accept-Language": "en-US,en;q=0.9",
+        Accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+        Connection: "keep-alive",
       },
     });
     const html = response.data;
